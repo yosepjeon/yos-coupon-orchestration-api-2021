45b03ebf43a18c4fcd516c7be139fb59aa673698
yosepjeon <enekelx1@naver.com>
2021-10-18 23:53:11 +0900

    쿠폰 SAGA 기능 구현 완료

--- src/main/kotlin/com/yosep/coupon/coupon/data/jpa/dto/ProductDiscountCouponDtoForCreation.kt
+++ src/main/kotlin/com/yosep/coupon/coupon/data/jpa/dto/ProductDiscountCouponDtoForCreation.kt
@@ -2,7 +2,6 @@ package com.yosep.coupon.coupon.data.jpa.dto
 
 import com.yosep.coupon.coupon.data.jpa.vo.CouponDiscountVo
 import com.yosep.coupon.coupon.data.jpa.vo.CouponStockVo
-import javax.validation.constraints.Min
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
